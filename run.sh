0d155677e911537923a2b102b20077687ccb7010
linyulin <linyulin@chandashi.com>
2023-05-06 10:53:20 +0800

    feat: 运行脚本

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+workdir=$(cd "$(dirname "$0")";pwd) || exit
+printf "current work directory: %s\n" "$workdir"
+cd "$workdir" || exit
+printf "create docker directory...\n"
+mkdir -p /mnt/docker
+printf "copy resources into new directory...\n"
+cp -R ./ /mnt/docker
+cd /mnt/docker
+printf "running docker compose...\n"
+docker-compose up -d
+printf "finish\n"
