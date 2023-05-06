b357ad1d941c58c0cf4131805ad3e63f66d8679e
linyulin <linyulin@chandashi.com>
2023-05-06 10:56:18 +0800

    feat: PHP环境处理

---
+++ install_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-compose
